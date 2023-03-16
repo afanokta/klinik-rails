30d62b7024f9caa28b922854f40dda3753541c0c
afanokta <afanoktafianto99@gmail.com>
2023-03-16 15:57:23 +0700

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :departements
-  resources :schedules
+
+  resources :user
+  resources :medical_history
+  resources :schedule
+  resources :appointment
+  resources :department
+  namespace 'user' do
+  end
+  get '/doctors', to: 'user#doctor'
+  post '/login', to: 'user#login'
 end
