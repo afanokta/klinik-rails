ec521228be3d9238160e1fe51d19f71cbb2d55d3
afanokta <afanoktafianto99@gmail.com>
2023-03-16 14:28:03 +0700

    adding users stuff

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  belongs_to :department, optional: true
+  has_many :schedules, class_name: 'Schedule', foreign_key: 'doctor_id'
+  has_many :appointments, class_name: 'Appointment', foreign_key: 'patient_id'
+
+  scope :doctor, -> { where(role: 'doctor') }
+
+  enum role: {
+    patient: 0,
+    doctor: 1,
+    admin: 2
+  }
+
+  enum gender: {
+    male: 0,
+    female: 1
+  }
+
+  def new_attr
+    {
+      id: self.id,
+      username: self.username,
+      email: self.email,
+      name: self.name,
+      dob: self.dob,
+      gender: self.gender,
+      phone_number: self.phone_number,
+      department: self.department&.new_attr,
+      role: self.role
+    }
+  end
+
+  def doc_attr
+    {
+      email: self.email,
+      dob: self.dob,
+      gender: self.gender,
+      phone_number: self.phone_number,
+      department: self.department.name,
+      role: self.role
+    }
+  end
+end
