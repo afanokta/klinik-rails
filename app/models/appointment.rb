0c25669e698e41c9b1340d165d31b19bd523130f
afanokta <afanoktafianto99@gmail.com>
2023-03-17 14:57:38 +0700

    add appointment display_all

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,15 +1,15 @@
 class Appointment < ApplicationRecord
   belongs_to :schedule
-  belongs_to :patient, class_name: "User", foreign_key: "patient_id"
+  belongs_to :patient, class_name: 'User', foreign_key: 'patient_id'
   has_one :medical_history
 
   validates_uniqueness_of :schedule_id, scope: [:patient_id, :date]
 
   scope :join_schedule, -> { joins(:schedule) }
-  scope :by_doctor, ->(current_user) { join_schedule.where(schedule: { doctor_id: current_user.id }) }
-  scope :doctor_detail, ->(app, current_user) { by_doctor(current_user).where(id: app.id) }
+  scope :by_doctor, ->(doctor_id) { join_schedule.where(schedule: { doctor_id: doctor_id }) }
+  scope :doctor_detail, ->(app, doctor_id) { by_doctor(doctor_id).where(id: app.id) }
 
-  scope :by_patient, ->(current_user) { where(patient_id: current_user.id) }
+  scope :by_patient, ->(patient_id) { where(patient_id: patient_id) }
   scope :patient_detail, ->(current_user) { where(patient_id: current_user.id) }
 
   enum status: {
@@ -28,6 +28,23 @@ class Appointment < ApplicationRecord
     }
   end
 
-  def self.filter
+  def self.display_all(params)
+    start_date = params[:start_date]
+    end_date = params[:end_date]
+    doc_id = params[:doctor_id].to_i if params[:doctor_id].present?
+    patient_id = params[:patient_id].to_i if params[:patient_id].present?
+    start_date = start_date.present? ? start_date : '2000-01-01'
+    end_date = end_date.present? ? end_date : '2099-01-01'
+    # joins(:schedule).where(schedule: { doctor_id: doctor_id }) if doctor_id.present?
+
+    if patient_id.present?
+      return where('date BETWEEN ? AND ?', start_date, end_date).where(patient_id: patient_id)
+    end
+
+    if doc_id.present?
+      return where('date BETWEEN ? AND ?', start_date, end_date).by_doctor(doc_id)
+    end
+
+    where('date BETWEEN ? AND ?', start_date, end_date)
   end
 end
